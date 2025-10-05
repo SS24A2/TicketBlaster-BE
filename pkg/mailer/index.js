1d955c2f1008bc908dde0e510092adabe9a522fa
SS24A2 <ana.janevska21@yahoo.com>
2025-10-05 21:29:36 +0200

    modify sendMail function for tickets

--- pkg/mailer/index.js
+++ pkg/mailer/index.js
@@ -7,6 +7,9 @@ const config = require("../config");
 const mailgun = new Mailgun(formData);
 let mg = null
 
+const fsPromises = require('fs').promises;
+const { createAllTickets } = require("../tickets/createAllTickets")
+
 const mailTemplates = {
     PASSWORD_RESET: {
         title: "Your password reset link has been generated",
@@ -16,12 +19,13 @@ const mailTemplates = {
         title: "Welcome to our website",
         template: "welcome.ejs",
     },
+    TICKETS: {
+        title: "Your Ticket Purchase Is Complete!",
+        template: "tickets.ejs",
+    }
 };
 
-// sendMail("123@gmail.com", "WELCOME", { first_name: "123", last_name: "123", email: "123@gmail.com" });
-// sendMail("123@gmail.com", "PASSWORD_RESET", { username: "123", link: "http://test.com" });
-
-const sendMail = async (to, type, data) => {
+const sendMail = async (to, type, data, ticketsIdArray) => {
     try {
         if (!mg) {
             mg = mailgun.client({ username: "api", key: config.getSection("mailgun").api_key })
@@ -31,14 +35,30 @@ const sendMail = async (to, type, data) => {
         const templatePath = `${__dirname}/../../pkg/mailer/email_templates/${mailTemplates[type].template}`
 
         let content = null
-        content = await ejs.renderFile(templatePath, { ...data })  //TBC!!
+        content = await ejs.renderFile(templatePath, { data })
 
-        return await mg.messages.create(config.getSection("mailgun").domain, {
+        let messageParams = {
             from: `${config.getSection("mailgun").sender_email} <mailgun@${config.getSection("mailgun").domain}>`,
             to: to,
             subject: title,
             html: content,
-        });
+        }
+
+        if (type === "TICKETS") {
+            const tickets = await createAllTickets(ticketsIdArray)
+
+            let attachments = []
+            for (let i = 0; i < tickets.length; i++) {
+                attachments.push({ filename: `Ticket_No.${i + 1}-${tickets[i].eventName.replace(/ /g, "_")}.html`, data: tickets[i].ticket })
+            }
+            messageParams.attachment = attachments;
+
+            const imageFile = await fsPromises.readFile(`${__dirname}/email_templates/logo.png`)
+            const file = { filename: 'logo.png', data: imageFile }
+            messageParams.inline = file;
+        }
+
+        return await mg.messages.create(config.getSection("mailgun").domain, messageParams);
     } catch (err) {
         throw err;
     }
