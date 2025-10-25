90e8cb0407cd016d76f4e21bd396c1abf9323a4b
SS24A2 <ana.janevska21@yahoo.com>
2025-10-25 20:16:31 +0200

    added ecommerce routes

--- services/ecommerce/index.js
+++ services/ecommerce/index.js
@@ -2,19 +2,22 @@ const express = require("express");
 
 const config = require("../../pkg/config");
 const db = require("../../pkg/db");
-// const cars = require("./handlers/events");
+
+const { checkAvailableTicketsForEvent, selectCheckout, buyTickets, viewTicketsHistory, cancelCheckout, printTickets } = require("./handlers/ecommerce");
 
 db.init();
 
 const api = express();
 api.use(express.json())
 
-// api.get("/api/v1/cars", cars.getVehicle);
-// api.post("/api/v1/cars", cars.createVehicle);
-// api.put("/api/v1/cars/:id", cars.updateVehicle);
-// api.delete("/api/v1/cars/:id", cars.removeVehicle);
+api.get("/api/v1/ecommerce/:id", checkAvailableTicketsForEvent);
+api.put("/api/v1/ecommerce/checkout", selectCheckout);
+api.put("/api/v1/ecommerce/checkout/cancel", cancelCheckout);
+api.put("/api/v1/ecommerce/payment", buyTickets);
+api.get("/api/v1/ecommerce/tickets/history", viewTicketsHistory);
+api.get("/api/v1/ecommerce/tickets/print", printTickets);
 
-// Port 10004
+// Port 10003
 api.listen(config.getSection("services").ecommerce.port, (err) => {
   if (err) {
     console.log("error", err);
